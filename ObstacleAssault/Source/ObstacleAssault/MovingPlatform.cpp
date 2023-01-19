4482cdbec0a36c8205bd6f7d23d7c3ddfa9d200d
Jiachen Tang <tjc9606ok@gmail.com>
2023-01-19 00:26:37 -0500

    ~ chapter 3 completed

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -16,6 +16,12 @@ void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
 
+	StartLocation = GetActorLocation();
+
+	FString Name = GetName();
+
+	UE_LOG(LogTemp, Display, TEXT("Begin play: %s"), *Name);
+
 }
 
 // Called every frame
@@ -23,16 +29,42 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Move platform forwards
-		// Get current location
-	FVector CurrentLocation = GetActorLocation();
-		// Add vector to that location
-	CurrentLocation += PlatformVelocity * DeltaTime;
-		// Set the location
-	SetActorLocation(CurrentLocation);
-	// Send platform back if gone too far
-		// Check how far we have moved
-		// reverse direction of moving if gone too far
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
 
 }
 
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	if (ShouldPlatformReturn())
+	{
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		StartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(StartLocation);
+
+		PlatformVelocity *= -1;
+	}
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();
+		CurrentLocation += PlatformVelocity * DeltaTime;
+		SetActorLocation(CurrentLocation);
+	}
+
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	// UE_LOG(LogTemp, Display, TEXT("%s rotating..."), *GetName());
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
+}
+
+bool AMovingPlatform::ShouldPlatformReturn() const
+{
+	return GetDistanceMoved() > MoveDistance;
+}
+
+float AMovingPlatform::GetDistanceMoved() const
+{
+	return FVector::Dist(StartLocation, GetActorLocation());
+}
